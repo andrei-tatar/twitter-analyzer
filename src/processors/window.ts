a736170856f149297d7f2b4a3ed6fbe2a20d9820
Andrei Tatar <tatarandrei@gmail.com>
2020-03-09 13:36:31 +0200

    start a new window when value changes

--- src/processors/window.ts
+++ src/processors/window.ts
@@ -1,9 +1,10 @@
 import { interval, Observable, OperatorFunction } from 'rxjs';
-import { switchMap, window } from 'rxjs/operators';
+import { startWith, switchMap, window } from 'rxjs/operators';
 
 export function windowDynamicTime<T>(seconds$: Observable<number>): OperatorFunction<T, Observable<T>> {
     const window$ = seconds$.pipe(
         switchMap(seconds => interval(seconds * 1000)),
+        startWith(0),
     );
 
     return source => source.pipe(
