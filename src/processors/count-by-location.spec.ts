bc8cf1b5b4c4fe7b068d54670ca93845d1f45850
Andrei Tatar <tatarandrei@gmail.com>
2020-03-09 14:09:37 +0200

    add a few tests

---
+++ src/processors/count-by-location.spec.ts
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import { EMPTY, from, Subject } from 'rxjs';
+import { first, last } from 'rxjs/operators';
+import { countByLocation } from './count-by-location';
+import { MessageWithLocation } from './geocode-message';
+
+describe('count-by-location', () => {
+    const sut = countByLocation();
+
+    it('should start with an empty value', async () => {
+        // act
+        const result = await sut(EMPTY).toPromise();
+
+        // assert
+        expect(result).deep.eq({});
+    });
+
+    it('should aggregate by location.country', async () => {
+
+        // arrange
+        const events = from<MessageWithLocation[]>([{
+            location: {
+                coords: [1, 2],
+                country: 'RO',
+            },
+            message: null as any,
+        }, {
+            location: {
+                coords: [1, 2],
+                country: 'RO',
+            },
+            message: null as any,
+        }, {
+            location: {
+                coords: [1, 2],
+                country: 'BE',
+            },
+            message: null as any,
+        }]);
+
+        // act
+        const result = await sut(events).toPromise();
+
+        // assert
+        expect(result).deep.eq({ 'RO': 2, 'BE': 1 });
+    });
+
+    it('should use empty string when location is null', async () => {
+        // arrange
+        const events = from<MessageWithLocation[]>([{
+            location: null,
+            message: null as any,
+        }]);
+
+        // act
+        const result = await sut(events).toPromise();
+
+        // assert
+        expect(result).deep.eq({ '': 1 });
+    });
+
+    it('should complete only when source completes', async () => {
+        // arrange
+        const events = new Subject<MessageWithLocation>();
+        let complete = false;
+
+        // act
+        sut(events).subscribe(() => { }, _ => { }, () => complete = true);
+
+        // assert
+        expect(complete).to.eq(false);
+        events.next({} as any);
+        expect(complete).to.eq(false);
+        events.complete();
+        expect(complete).to.eq(true);
+    });
+});
