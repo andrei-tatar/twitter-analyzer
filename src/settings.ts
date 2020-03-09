b4dd399016324257c2dc4b66edc0e2a766ccab1e
Andrei Tatar <tatarandrei@gmail.com>
2020-03-09 15:28:03 +0200

    crude validation

--- src/settings.ts
+++ src/settings.ts
@@ -12,8 +12,13 @@ export function settingsMap<T>(mapper: (s: Settings) => T): Observable<T> {
 
 export async function updateSettings(update: Partial<Settings>) {
     const settings = await settings$.pipe(first()).toPromise();
-    const updated = { ...settings, ...update };
-    update$.next(updated);
+    const clone: any = { ...settings };
+    for (const [key, value] of Object.entries(update)) {
+        if (key in clone && typeof value === typeof clone[key]) {
+            clone[key] = value;
+        }
+    }
+    update$.next(clone);
 }
 
 export const settings$ = update$.pipe(
