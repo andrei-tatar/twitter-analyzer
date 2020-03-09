65f530e2c9767e03052bc80693e4c2b4d1d22e7d
Andrei Tatar <tatarandrei@gmail.com>
2020-03-09 12:08:59 +0200

    add config

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const TWITTER = {
+    consumer_key: 'ADD_YOUR_KEY',
+    consumer_secret: 'ADD_YOUR_KEY',
+    access_token: 'ADD_YOUR_KEY',
+    access_token_secret: 'ADD_YOUR_KEY',
+};
+
+export const GOOGLE_API_KEY = 'ADD_YOUR_KEY';
